c6986a9179f666419b988f576c94711a3716f7bb
G.k Mahalakshmi <102350492+mahalakshmiganugula12@users.noreply.github.com>
2022-10-04 20:37:55 +0530

    import java.util.Scanner; class prime {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[];         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)
                x[i]=sc.nextInt();
            for(int i=0;i<n-1;i++)
            {
                if(x[i]>=x[i+1])
                {
                    System.out.print("no");
                    System.exit(0);
                }
                
            }
            System.out.println("yes");
        }
    }

--- Ascending_array.java
+++ Ascending_array.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
-class Dec
+class prime
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,i,x[],count=0;
+        int n,x[];
         n=sc.nextInt();
         x=new int[n];
-        for(i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++)
+        
             x[i]=sc.nextInt();
-        }
-        for(i=0;i<n-1;i++)
+        for(int i=0;i<n-1;i++)
         {
             if(x[i]>=x[i+1])
             {
-                System.out.println("no");
+                System.out.print("no");
                 System.exit(0);
             }
+            
         }
         System.out.println("yes");
-        
     }
 }
