7c36ef205dccb8502927a6d8c0e623f1cb9bf6eb
G.k Mahalakshmi <102350492+mahalakshmiganugula12@users.noreply.github.com>
2022-12-09 14:33:23 +0530

    import java.util.Scanner; class string1 {   public static void main(String args[ ])   {      Scanner sc=new Scanner(System.in);      String s1=sc.nextLine( );      int c=0;      for(int i=0;i<s1.length( );i++)      {        char ch=s1.charAt(i);        if((ch>=65) && (ch<=90))        {            c++;        }
         }
         System.out.println(c);
      }
    }

---
+++ uppercase_letters_in_a_string.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class string1
+{
+  public static void main(String args[ ])
+  {
+     Scanner sc=new Scanner(System.in);
+     String s1=sc.nextLine( );
+     int c=0;
+     for(int i=0;i<s1.length( );i++)
+     {
+       char ch=s1.charAt(i);
+       if((ch>=65) && (ch<=90))
+       {
+           c++;
+       }
+       
+       
+          
+     
+     }
+     System.out.println(c);
+  }
+}
